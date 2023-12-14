a63fa48d2afad3232c961b79bbd560d2fbc44de3
Payakorn Saksuriya <payakornn@gmail.com>
2023-12-14 21:11:44 +0700

    add run

--- run.jl
+++ run.jl
@@ -1,14 +1,15 @@
-# try using CD catch e; import Pkg; Pkg.activate("."); Pkg.instantiate(); using CD end
+try using CD catch e; import Pkg; Pkg.activate("."); Pkg.instantiate(); using CD end
 
-# CD.start();
+u, v, elapsed_time, error_save, iteration = CD.start();
 
+# using Plots
 
-# u, v, time, er, iteration = CD.sys_stream(0.01, 0.01, 0.01, 100, max_iteration=500)
-m = minimum(v)
-M = maximum(v)
+# # u, v, time, er, iteration = CD.sys_stream(0.01, 0.01, 0.01, 100, max_iteration=100)
+# m = minimum(v)
+# M = maximum(v)
 
-sc1 = collect(m:-m/20:0)
-sc2 = collect(0:m/50:M)
-sc = [sc1; sc2]
-Plots.savefig(Plots.contour(u, fill=false, levels=sc), "test_u.pdf");
-Plots.savefig(Plots.contour(v, fill=true, levels=sc, color=:turbo), "test_v.pdf");
+# sc1 = collect(m:-m/20:0)
+# sc2 = collect(0:m/50:M)
+# sc = [sc1; sc2]
+# Plots.savefig(Plots.contour(u, fill=false, levels=sc), "test_u.pdf");
+# Plots.savefig(Plots.contour(v, fill=false, levels=sc, color=:turbo), "test_v.pdf");
--- src/input.jl
+++ src/input.jl
@@ -6,7 +6,7 @@ using REPL.TerminalMenus
 function start()
 
     options = [
-        "hx=hy=τ=0.1, Re=100, maxiter=10",
+        "hx=hy=τ=0.1, Re=100, maxiter=100",
         "hx=hy=τ=0.01, Re=100, maxiter=100",
         "Manually Input"
     ]
@@ -19,13 +19,13 @@ function start()
         if choice == 1
             hx=hy=tau=0.1
             Re=100
-            maxiter=10
-            solve(hx, hy, tau, Re; maxiter=maxiter);
+            max_iteration=100
+            u, v, elapsed_time, error_save, iteration = sys_stream(hx, hy, tau, Re; max_iteration=max_iteration);
         elseif choice == 2
             hx=hy=tau=0.01
             Re=100
-            maxiter=100
-            solve(hx, hy, tau, Re; maxiter=maxiter);
+            max_iteration=100
+            u, v, elapsed_time, error_save, iteration = sys_stream(hx, hy, tau, Re; max_iteration=max_iteration);
         elseif choice == 3
             print("Input hx: ")
             hx = parse(Float64, readline())
@@ -40,14 +40,13 @@ function start()
             Re = parse(Float64, readline())
             
             print("Input max iter: ")
-            maxiter = parse(Int64, readline())
+            max_iteration = parse(Int64, readline())
 
-            solve(hx, hy, tau, Re; maxiter=maxiter);
+            u, v, elapsed_time, error_save, iteration = sys_stream(hx, hy, tau, Re; max_iteration=max_iteration);
         end
     else
         println("Canceled.")
     end
-
-    # hx, hy, tau, Re; maxiter=10
+    return u, v, elapsed_time, error_save, iteration
 end
 
